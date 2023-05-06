6aa2bb3dab428ca44425aa5f49062c8735ab7f56
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-05-06 17:39:06 -0300

    feat: add my go lib 2 dep

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/filipe1309/my-go-library
 
 go 1.20
+
+require github.com/filipe1309/my-go-library-2 v1.0.0
--- mygolib.go
+++ mygolib.go
@@ -2,8 +2,15 @@ package mygolibrary
 
 import (
 	"fmt"
+
+	mygolib2 "github.com/filipe1309/my-go-library-2"
 )
 
 func HelloFrom110() {
 	fmt.Println("Hello from My Go Library v1.1.0")
+}
+
+func HelloFromL1v120() {
+	fmt.Println("Hello from My Go Library v1.2.0")
+	mygolib2.HelloFromL2v100()
 }
